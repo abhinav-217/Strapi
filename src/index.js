3a4822465d4679c0648777727b58b070ba1effe8
abhinav-217 <abhinavsaxena217@gmail.com>
2024-05-02 04:19:17 +0530

    Final changes

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ module.exports = {
       socket.on('chat message', async (msg) => {
         var axios = require("axios");
         const { GoogleGenerativeAI } = require("@google/generative-ai");
-        const genAI = new GoogleGenerativeAI('AIzaSyBd5RuKD9mAmb4XHNyxagDx13p4uYXi3Oo');
+        const genAI = new GoogleGenerativeAI('API_KEY');
         async function run(query) {
           // For text-only input, use the gemini-pro model
           const model = genAI.getGenerativeModel({ model: "gemini-pro"});
